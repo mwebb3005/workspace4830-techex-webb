8403cf111ce803a187e3e07bacc68ca5944e86ae
mwebb3005 <90664746+mwebb3005@users.noreply.github.com>
2023-03-18 18:01:17 -0500

    Delete webprojectcsci4830-techex-webb directory

--- webprojectcsci4830-techex-webb/src/main/java/util/Info.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public interface Info {
-   public String projectName = "webprojectcsci4830-techex-webb";
-   public String searchWebName = "search_webb.html";
-   public String insertWebName = "insert_webb.html";
-}
